42d6c58a47b8e8766d1f5b955d2e257496e480e1
ajay <ajaypathak2527@gmail.com>
2024-12-16 20:52:09 +0530

    cors url update

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5000'],
+    origin: ['http://localhost:5173', 'https://abc.com'],
   }
 })
 
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:5173,http://localhost:5000,https://*.awsapprunner.com,https://staginga360h.com',
+  origin: 'http://localhost:5000,https://*.awsapprunner.com,https://staginga360h.com,http://localhost:5173',
   credentials: true
 }))
 app.use(express.urlencoded({extended: true}))
